4eea78b29ddce69cc97ccba66491de619e8d58b5
Khaled Ajaj <khaledajaj2026@gmail.com>
2024-04-18 11:36:40 -0700

    Submission for part1 section B.

--- part1/course_info/src/App.jsx
+++ part1/course_info/src/App.jsx
@@ -1,52 +1,49 @@
-const Header = ({ course }) => <h1>{course}</h1>;
-const Part = ({ part, exercise }) => (
+const Header = ({ course }) => <h1>{course.name}</h1>;
+const Part = ({ part }) => (
 	<p>
-		{part} {exercise}
+		{part.name} {part.exercises}
 	</p>
 );
-const Content = ({
-	part1,
-	part2,
-	part3,
-	exercises1,
-	exercises2,
-	exercises3,
-}) => (
+const Content = ({ course }) => (
 	<>
-		<Part part={part1} exercise={exercises1} />
-		<Part part={part2} exercise={exercises2} />
-		<Part part={part3} exercise={exercises3} />
+		<Part part={course.parts[0]} />
+		<Part part={course.parts[1]} />
+		<Part part={course.parts[2]} />
 	</>
 );
-const Total = ({ exercise1, exercise2, exercise3 }) => (
-	<p>Number of exercises {exercise1 + exercise2 + exercise3}</p>
+const Total = ({ course }) => (
+	<p>
+		Number of exercises{' '}
+		{course.parts[0].exercises +
+			course.parts[1].exercises +
+			course.parts[2].exercises}
+	</p>
 );
 
 const App = () => {
-	const course = 'Half Stack application development';
-	const part1 = 'Fundamentals of React';
-	const exercises1 = 10;
-	const part2 = 'Using props to pass data';
-	const exercises2 = 7;
-	const part3 = 'State of a component';
-	const exercises3 = 14;
+	const course = {
+		name: 'Half Stack application development',
+		parts: [
+			{
+				name: 'Fundamentals of React',
+				exercises: 10,
+			},
+			{
+				name: 'Using props to pass data',
+				exercises: 7,
+			},
+			{
+				name: 'State of a component',
+				exercises: 14,
+			},
+		],
+	};
 
 	return (
 		<>
 			<Header course={course} />
-			<Content
-				part1={part1}
-				part2={part2}
-				part3={part3}
-				exercises1={exercises1}
-				exercises2={exercises2}
-				exercises3={exercises3}
-			/>
-			<Total
-				exercise1={exercises1}
-				exercise2={exercises2}
-				exercise3={exercises3}
-			/>
+			<Content course={course} />
+			<Total course={course} />
 		</>
 	);
 };

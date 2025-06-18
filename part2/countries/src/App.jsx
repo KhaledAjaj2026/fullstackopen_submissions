6343293d16c24fad66dc30f2428078c8aa6c76fc
Khaled Ajaj <khaledajaj@OG-M3-MBA.local>
2025-06-18 15:11:22 -0700

    Exercise 2.18

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -4,7 +4,6 @@ import countries from "./directory/countries"
 function App() {
   // state variable containing list of countries
   const [countryList, setCountryList] = useState([]);
-  const [searchInput, setSearchInput] = useState("");
 
   // fetch list of countries from API, initialize countryList
   useEffect(() => {
@@ -13,29 +12,38 @@ function App() {
   
   // handle user input by updating state variable 'searchInput'
   const handleSearchInput = (event) => {
-    setSearchInput(event.target.value);
-
-    if(searchInput) {
-      const newCountryList = countries.getCountries().then(res => {
-        return res.data.filter(country => country.name.common.includes(searchInput))
-      });
+    countries.getCountries().then(res => {
+      const newCountryList = res.data.filter(country => country.name.common.includes(event.target.value))
       setCountryList(newCountryList);
-    }
+    });
   }
-
-  console.log("country list: ", countryList);
 
   return (
     <>
-    <form>
-     <label htmlFor="country-search">find countries</label>
-     <input id="country-search" onChange={handleSearchInput} />
-    </form>
-    <div>
-      {countryList.length > 0 ? 
-        countryList.map((country, i) => <p key={i}>{country.name.common}</p>) 
+      <form>
+      <label htmlFor="country-search">find countries</label>
+      <input id="country-search" onChange={handleSearchInput} />
+      </form>
+      <div>
+        {countryList.length === 1 ? 
+          <div>
+            <h2>{countryList[0].name.common}</h2>
+            <p>Capital {countryList[0].capital[0]}</p>
+            <p>Area {countryList[0].area}</p>
+            <h3>Languages</h3>
+            <ul>
+              {Object.values(countryList[0].languages).map((lang, i) => <li key={i}>{lang}</li>)}
+            </ul>
+            <img 
+              src={countryList[0].flags.svg} 
+              style={{width: "15rem", height: "auto"}} 
+              alt={`flag of ${countryList[0].name.common}`}
+            />
+          </div>
+        : countryList.length <= 10 ? 
+          countryList.map((country, i) => <p key={i}>{country.name.common}</p>) 
         : <p>Too many matches, specify another filter</p>}
-    </div>
+      </div>
     </>
   )
 }

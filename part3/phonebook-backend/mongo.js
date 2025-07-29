f865a6ced0c38b9b0c61fa9fdc5c2052ce4f6023
Khaled Ajaj <khaledajaj@OG-M3-MBA.local>
2025-07-29 15:56:00 -0700

    Exercise 3.12

---
+++ part3/phonebook-backend/mongo.js
@@ -0,0 +1,49 @@
+// Introduce mongoose into project
+const mongoose = require('mongoose');
+// Introduce access to argv helper
+const {argv} = require('node:process')
+// Grab password from process argument
+const password = process.argv[2];
+// Embed password into Mongo cluster URL
+const url = `mongodb+srv://kajaj2026:${password}@cluster0.6eq5mib.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Set strictQuery to false
+mongoose.set('strictQuery', false);
+// connect Mongoose to the cluster via url
+mongoose.connect(url);
+
+// Schema for creating a new "Person" model
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+});
+
+// Create new "Person" model using personSchema
+const Person = mongoose.model('Person', personSchema);
+
+// // Log argv to better understand its behavior
+// argv.forEach(item => {
+//     console.log(item);
+// });
+
+if(process.argv.length == 3) {
+    // Fetch all persons from cluster, log to console, then terminate connection
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`);
+        });
+        mongoose.connection.close();
+    });
+} else if(process.argv.length == 5) {
+    // Create new person by using Person schema
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4],
+    })
+
+    // Save new person to cluster, then terminate connection
+    person.save().then(result => {
+        console.log(`Added ${result.name} number ${result.number} to phonebook!`);
+        mongoose.connection.close();
+    });
+}
